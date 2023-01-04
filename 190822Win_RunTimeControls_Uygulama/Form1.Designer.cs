261c74c6b0a18d4aecf5052f2ed4f9aac2ad2e13
murat.dogan2 <murat.dogan2@ERKHBN0096>
2023-01-04 16:13:16 +0300

    Add project files.

---
+++ 190822Win_RunTimeControls_Uygulama/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace _190822Win_RunTimeControls_Uygulama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nudAdet = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbTipler = new System.Windows.Forms.ComboBox();
+            this.btnUret = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAdet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(7, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Adet:";
+            // 
+            // nudAdet
+            // 
+            this.nudAdet.Location = new System.Drawing.Point(10, 31);
+            this.nudAdet.Name = "nudAdet";
+            this.nudAdet.Size = new System.Drawing.Size(99, 22);
+            this.nudAdet.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(112, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tip:";
+            // 
+            // cmbTipler
+            // 
+            this.cmbTipler.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbTipler.FormattingEnabled = true;
+            this.cmbTipler.Items.AddRange(new object[] {
+            "Button",
+            "TextBox",
+            "ComboBox",
+            "PictureBox",
+            "ListBox"});
+            this.cmbTipler.Location = new System.Drawing.Point(115, 31);
+            this.cmbTipler.Name = "cmbTipler";
+            this.cmbTipler.Size = new System.Drawing.Size(184, 24);
+            this.cmbTipler.TabIndex = 3;
+            // 
+            // btnUret
+            // 
+            this.btnUret.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnUret.Location = new System.Drawing.Point(305, 31);
+            this.btnUret.Name = "btnUret";
+            this.btnUret.Size = new System.Drawing.Size(116, 26);
+            this.btnUret.TabIndex = 4;
+            this.btnUret.Text = "ÜRET";
+            this.btnUret.UseVisualStyleBackColor = true;
+            this.btnUret.Click += new System.EventHandler(this.btnUret_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 61);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(409, 309);
+            this.flowLayoutPanel1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 386);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.btnUret);
+            this.Controls.Add(this.cmbTipler);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nudAdet);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudAdet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown nudAdet;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbTipler;
+        private System.Windows.Forms.Button btnUret;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ 190822Win_RunTimeControls_Uygulama/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _190822Win_RunTimeControls_Uygulama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbTipler.SelectedIndex = 0;//comboboxta ilk seçenek seçili halde gelmesini istiyoruz
+        }
+
+        private void btnUret_Click(object sender, EventArgs e)
+        {
+            flowLayoutPanel1.Controls.Clear();
+            int adet =Convert.ToInt32(nudAdet.Value);
+            for (int i = 0; i < adet; i++)
+            {
+                switch (cmbTipler.Text)
+                {
+                    case "Button":
+                        Button btn=new Button();
+                        btn.Height = 30;
+                        btn.Width = 80;
+                        btn.Text = string.Format("Button{0}", i + 1);
+                        btn.Font = new Font("Tahoma", 10) ;
+                        btn.BackColor = Color.Gray;
+                        btn.Click += Btn_Click;
+                        btn.MouseHover += Btn_MouseHover;
+                        btn.MouseLeave += Btn_MouseLeave;
+                        btn.MouseEnter += Btn_MouseEnter;
+                        flowLayoutPanel1.Controls.Add(btn);
+                        break;
+
+                    case "ComboBox":
+                        //Girilen adet kadar combobox üretilecek.
+                        //Her combobox ın itemlerine eleman eklenecek.
+                        //SelectedIndexChanged eventi tanımlanacak.
+                        //Bu event te seçilecek elemanın ismi messageBox ile gösterilecek.
+                        //Comboboxların dropdown  özelliği dropdownlist seçilecek.
+                        //Her combobox flowlayoutpanel e eklenecek.
+                        ComboBox cmb = new ComboBox();
+                        cmb.Items.Add("Item 1");
+                        cmb.Items.Add("Item 2");
+                        cmb.Items.Add("Item 3");
+                        cmb.DropDownStyle= ComboBoxStyle.DropDownList;
+                        cmb.SelectedIndexChanged += Cmb_SelectedIndexChanged;
+                        flowLayoutPanel1.Controls.Add(cmb);
+
+                        break;
+                    case "PictureBox": break;
+                    case "TextBox": break;
+                    case "ListBox": break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void Cmb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox secilen = (ComboBox)sender;
+            MessageBox.Show(secilen.Text);
+        }
+
+        private void Btn_MouseEnter(object sender, EventArgs e)
+        {
+            Button tik = (Button)sender;
+            tik.BackColor = Color.Green;//Mouse butonun üzerine gelirse rengini değiştirir. mouseHover dan hızlı çalışıyor.
+        }
+
+       private void Btn_MouseLeave(object sender, EventArgs e)
+        {
+           Button tiklanan=(Button)sender;//cast etme
+            tiklanan.BackColor = Color.Gray;//mouse butonun üzerinden ayrılırsa gri yap
+        }
+       
+        private void Btn_MouseHover(object sender, EventArgs e)
+        {
+            Button tiklanan = sender as Button;//cast etme
+           //tiklanan.BackColor = Color.Red;
+           //mouse butonun üzerine gelirse kırmızı yap
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Button tiklanan=sender as Button;
+            MessageBox.Show(string.Format("{0}'e Tıklandı.",tiklanan.Text));
+        }
+    }
+}
